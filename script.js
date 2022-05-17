46575da806e6315f0bd5900bc2372da047bcc853
maestro44 <tomunsigned@gmail.com>
2022-05-16 21:33:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.2/build/threejs-toys.module.cdn.min.js'
 
 
 // var with an array of colors
@@ -33,16 +32,3 @@ function randomGradient() {
 
 
 
-neonCursor({
-  el: document.getElementById('app'),
-  shaderPoints: 16,
-  curvePoints: 80,
-  curveLerp: 0.5,
-  radius1: 5,
-  radius2: 30,
-  velocityTreshold: 10,
-  sleepRadiusX: 100,
-  sleepRadiusY: 100,
-  sleepTimeCoefX: 0.0025,
-  sleepTimeCoefY: 0.0025
-})
